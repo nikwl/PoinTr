e2da6815c63b5652a890afa0cfd8ff1400321945
nikwl <nikolas.lamb@gmail.com>
2022-06-02 16:09:55 +0000

    d

--- tools/runner.py
+++ tools/runner.py
@@ -12,8 +12,6 @@ from utils.AverageMeter import AverageMeter
 from utils.metrics import Metrics
 from extensions.chamfer_dist import ChamferDistanceL1, ChamferDistanceL2
 
-from .mesh import render
-from PIL import Image
 
 def run_net(args, config, train_writer=None, val_writer=None):
     logger = get_logger(args.log_name)
